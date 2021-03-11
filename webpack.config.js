22692b5c9fb0cb4bedca2969350aa986f295d905
kesiahp18 <kesiahp18@gmail.com>
2021-03-11 15:04:36 -0500

    optimize images with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,34 @@ const webpack = require("webpack");
 
 module.exports = 
 {   
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [{
+            test: /\.jpg$/i,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name(file) {
+                        return "[path][name].[exit]"
+                    },
+                    publicPath: function(url) {
+                        return url.replace("../", "/assets/")
+                    }
+                }
+            },
+            {
+                loader: 'image-webpack-loader'
+            }]
+        }]
     },
     plugins: [
         new webpack.ProvidePlugin({
